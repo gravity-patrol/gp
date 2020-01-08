63d23b6e5dfef42ed817426be46010b8c16bd436
gravity-patrol <59581553+gravity-patrol@users.noreply.github.com>
2020-01-08 16:48:14 +0100

    Update safe.c

--- scenarios/scenario_5/safe.c
+++ scenarios/scenario_5/safe.c
@@ -4,7 +4,7 @@
 // concatenate a and b in c. Overflow safe.
 void memcat(void *a, void *b, void *c, size_t a_len, size_t b_len, size_t c_len) {
 
-	assert(a_len + b_len < c_len); // Prevent overflows. Make sure the data actually fits in c
+	assert(a_len + b_len <= c_len); // Prevent overflows. Make sure the data actually fits in c
 
 	memcpy(c, a, a_len);
 
